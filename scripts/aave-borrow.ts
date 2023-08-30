155e9c2e9ab3a844e74cd65afa365c4855816ff8
Christos Kalivas <chrkalivas@gmail.com>
2023-08-30 19:41:28 +0300

    pool types and deps

--- scripts/aave-borrow.ts
+++ scripts/aave-borrow.ts
@@ -1,6 +1,17 @@
 import {ethers, network} from 'hardhat'
 import {getWeth} from './get-weth'
+import {Signer} from 'ethers'
 
+async function getPool(account: Signer) {
+  const poolAddressesProvider = await ethers.getContractAt(
+    'IPoolAddressesProvider',
+    '0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e',
+    account,
+  )
+
+  const poolAddress = await poolAddressesProvider.getPool()
+  const pool = await ethers.getContractAt('IPool', poolAddress, account)
+}
 async function main() {
   await getWeth()
   // Pool address: 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e
