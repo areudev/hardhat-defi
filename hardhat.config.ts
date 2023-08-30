28b70ccb69639a47e59c3c1323a01aea9a677009
Christos Kalivas <chrkalivas@gmail.com>
2023-08-30 19:04:30 +0300

    mainet init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,12 +10,16 @@ const SEPOLIA_URL = process.env.RPC_URL || ''
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ''
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ''
 
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
     },
     localhost: {
       url: 'http://127.0.0.1:8545/',
--- scripts/get-weth.ts
+++ scripts/get-weth.ts
@@ -2,10 +2,20 @@ import {ethers, network} from 'hardhat'
 async function getWeth() {
   // 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
   const deployer = (await ethers.getSigners())[0]
+  console.log('deployer', deployer.address)
+
   const iWeth = await ethers.getContractAt(
     'IWETH',
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
   )
+
+  const tx = await iWeth.deposit({
+    value: ethers.parseEther('0.1'),
+  })
+  await tx.wait(1)
+
+  const wethBalance = await iWeth.balanceOf(deployer)
+  console.log('wethBalance', ethers.formatEther(wethBalance))
 }
 
 export {getWeth}
